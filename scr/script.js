fe23756e7bdcb57adafce3a9bda5d5f2c97438af
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-03-15 22:22:05 +0700

    add a todo list for this project

--- scr/script.js
+++ scr/script.js
@@ -1,5 +1,15 @@
+// danh sach cong viec du kien:
+    // 1. Populate a board with tiles/mines
+    // 2. Left click on the titles
+        // a. reveal titles
+    // 3. Right click on titles
+        // a. Mark tiles
+    // check for win/lose
+
 document.addEventListener('DOMContentLoaded',() =>{
     // varialbles declaration
+    const dropdown = document.getElementById("board-size");
+    let boardSize = "8x8";
     let width = 10;
     let height = 10;
     let bombAmount = 20;
@@ -9,12 +19,21 @@ document.addEventListener('DOMContentLoaded',() =>{
 
     // initialize the game, the first move will always safe
     function init(){
-
+        setBoardSize();
+        createBoard();
     };
 
+    // set the size to the board
+    function setBoardSize(){
+        dropdown.addEventListener('change', (e) => {
+            boardSize = e.target.value;
+        });
+    }
+
     // main function to create the board
-    function createBoard(){
-        
+    function createBoard(boardSize){
+        let board = document.getElementById("board");
+        board.appendChild(document.createElement('button'))
     };
 
     // function to draw the board
