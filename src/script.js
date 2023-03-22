da95871d9b087b2d371823744f49e2e1095cf95e
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-03-22 09:37:43 +0700

    some fixing on the UI
    gio no hien cai bang len roi, lam not cai chec win lose nua la xong, con phan syle voi chinh kich thuoc bang thi de toi - Ho Gia Khang lam sau

--- src/script.js
+++ src/script.js
@@ -1,180 +1,167 @@
 // danh sach cong viec du kien:
-    // 1. Populate a board with tiles/mines
-    // 2. Left click on the titles
-        // a. reveal titles (Done)
-    // 3. Right click on titles
-        // a. Mark tiles (Partially done)
-    // 4. check for win/lose ngu
+// 1. Populate a board with tiles/mines
+// 2. Left click on the titles
+    // a. reveal titles (Done)
+// 3. Right click on titles
+    // a. Mark tiles (Partially done)
+// 4. check for win/lose
 
-document.addEventListener('DOMContentLoaded',() =>{
+const TILE_STATUSES = {
+    HIDDEN: "hidden",
+    MINE: "mine",
+    NUMBER: "number",
+    MARKED: "marked"
+}
+
+// initialize the game, the first move will always safe
+function init(){
     // varialbles declaration
-    const dropdown = document.getElementById("board-size");
-    const TILE_STATUSES = {
-        HIDDEN: 'hidden',
-        MINE: 'mine',
-        NUMBER: 'number',
-        MARKED: 'marked'
-    }
-    let board;
-    let boardSize = 8;
-    let width = 10;
-    let height = 10;
-    let bombAmount = 10;
-    let flags = 0;
-    let squares = [];
-    let isGameOver = false;
+    const BOARD_SIZE = setBoardSize();
+    const NUMBER_OF_MINES = 20
 
-    // initialize the game, the first move will always safe
-    function init(){
-        setBoardSize();
-        board = createBoard(boardSize, bombAmount);
-        const boardElement = document.querySelector('.board');
-        board.forEach(row => {
-            row.forEach(tile =>{
-                boardElement.append(tile.element);
-                tile.element.addEventListener("click", () => {
-                    revealTile(board,tile)
-                })
-                tile.element.addEventListener("contextmenu", e => {
-                    e.preventDefault()
-                    markTile(tile)
-                })
+    const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+    const boardElement = document.querySelector(".board")
+    const minesLeftText = document.querySelector("[data-mine-count]")
+    const messageText = document.querySelector(".subtext")
+    
+    board.forEach(row => {
+        row.forEach(tile => {
+            console.log(tile)
+            boardElement.append(tile.element)
+            tile.element.addEventListener("click", () => {
+                revealTile(board, tile)
+                //checkGameEnd()
+            })
+            tile.element.addEventListener("contextmenu", e => {
+                e.preventDefault()
+                markTile(tile)
+                //listMinesLeft()
             })
         })
-    };
+    })
+    boardElement.style.setProperty("--size", BOARD_SIZE)
+    minesLeftText.textContent = NUMBER_OF_MINES
+}
 
-    // set the size to the board
-    function setBoardSize(){
-        dropdown.addEventListener('change', (e) => {
-            boardSize = e.target.value;
-        });
+// set the size to the board
+function setBoardSize(){
+    const boardSize = 10;
+    // dropdown.addEventListener('change', (e) => {
+    //     boardSize = e.target.value;
+    // });
+    return boardSize;
+}
+
+export function createBoard(boardSize, numberOfMines){
+    const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+
+    for (let x = 0; x < boardSize; x++) {
+        const row = []
+        for (let y = 0; y < boardSize; y++) {
+        const element = document.createElement("div")
+        element.dataset.status = TILE_STATUSES.HIDDEN
+
+        const tile = {
+            element,
+            x,
+            y,
+            mine: minePositions.some(positionMatch.bind(null, { x, y })),
+            get status() {
+            return this.element.dataset.status
+            },
+            set status(value) {
+            this.element.dataset.status = value
+            },
+        }
+
+        row.push(tile)
+        }
+        board.push(row)
     }
 
-    // main function to create the board
-    function createBoard(boardSize, bombAmount){
-        minePositions = getMinepositions(boardSize,bombAmount)
-        const board = [];
-        for(let x = 0; x < boardSize; x++){
-            const row = [];
-            const element = document.createElement('div');
-            element.dataset.status = TILE_STATUSES.HIDDEN;
-            for(let y = 0; y < boardSize; y++){
-                const tile = {
-                    element, 
-                    mine: minePositions.some(positionMatch.bind(null,{x,y})),
-                    x, 
-                    y};
-            }
-            row.push(tile);
-        }
-        board.push(row);
+    return board
+};
 
-        return board;
-    };
-
-    // function to draw the board
-    function drawSquares(color, position){
-        this.squareColor = color;
-        this.position = position;
-    };
-
-    // function to handle the click event
-    function click(){
-
-    };
-
-    //function to mark tile 
-    function markTile(tile){
-        if(tile.status !== TILE_STATUSES.HIDDEN && 
-           tile.status !== TILE_STATUSES.MARKED){
-           return //end if it's already reveal
-        }
-
-        if (tile.status === TILE_STATUSES.MARKED){
-            tile.status = TILE_STATUSES.HIDDEN //if it's marked unmark it
-        }
-        else {tile.status = TILE_STATUSES.MARKED} //mark it otherwise
-    }   
-    
-    //function to reveal the clicked tile
-    function revealTile(board, tile){
-        if (tile.status !== TILE_STATUSES.HIDDEN){
-            return //end if tile is already reveal
-        }
-
-        if(tile.mine){
-            tile.status = TILE_STATUSES.MINE
-            return //end if the tile is a mine
-        }
-
-        tile.status = TILE_STATUSES.NUMBER
-        const adjacentTiles = adjacentTiles(board,tile)
-        const mines = adjacentTiles.filter(t => t.mine)
-
-        if (mines.length === 0){ //reveal nearby tiles that are not adjacent to any mines
-            adjacentTiles.forEach(revealTile.bind(null,board))
-        }
-        else{ //show number of mines nearby
-            tile.element.textContent = mines.length
-        }
+// function to mark tile 
+function markTile(tile){
+    if(tile.status !== TILE_STATUSES.HIDDEN && 
+        tile.status !== TILE_STATUSES.MARKED){
+        return //end if it's already reveal
     }
 
-    //function to check how many bombs are around the clicked tile
-    function adjacentTiles(board, {x,y}){
-        const tiles = []
-
-        for(let xOff = -1; xOff<=1; xOff++){ //nearby postions on left and right
-            for(let yOff = -1; yOff<=1; yOff++){ //nearby positions upper and bellow
-                const tile = board[x+xOff]?.[y+yOff]
-                if (tile){
-                 tiles.push(tile)
-                }
-            }
-        }
-
-        return tiles
+    if (tile.status === TILE_STATUSES.MARKED){
+        tile.status = TILE_STATUSES.HIDDEN //if it's marked unmark it
     }
-    // function to check whether the square is a bomb, or any bomb near by the square to calculate the display number
-    function checkSquare(){
+    else {tile.status = TILE_STATUSES.MARKED} //mark it otherwise
+}
 
-    };
+//function to reveal the clicked tile
+function revealTile(board, tile){
+    if (tile.status !== TILE_STATUSES.HIDDEN){
+        return //end if tile is already reveal
+    }
 
-    // function to add the flag onto the square
-    function addFlag(){
-        
-    };
+    if(tile.mine){
+        tile.status = TILE_STATUSES.MINE;
+        return //end if the tile is a mine
+    }
 
-    // function to draw the added flags
-    function drawFlags(position){
-        this.position = position;
-    };
+    tile.status = TILE_STATUSES.NUMBER;
+    const nearbyTiles = adjacentTiles(board,tile);
+    const mines = nearbyTiles.filter(t => t.mine);
 
-    //function to generate Mines positions
-    function getMinepositions(boardSize, bombAmount){
-        const positions = []
+    if (mines.length === 0){ //reveal nearby tiles that are not adjacent to any mines
+        nearbyTiles.forEach(revealTile.bind(null,board));
+    }
+    else{ //show number of mines nearby
+        tile.element.textContent = mines.length;
+    }
+}
 
-        //Loop until enough Mines
-        while (positions.length < bombAmount){ 
-            const position = {
-                x: random(boardSize),
-                y: random(boardSize),
-            }
+// function to calculate the number of bombs surround the square
+function adjacentTiles(board, {x,y}){
+    const tiles = []
 
-            if (!positions.some(p => positionMatch(p,position))){
-                positions.push(position)
+    for(let xOff = -1; xOff<=1; xOff++){ //nearby postions on left and right
+        for(let yOff = -1; yOff<=1; yOff++){ //nearby positions upper and bellow
+            const tile = board[x+xOff]?.[y+yOff]
+            if (tile){
+                tiles.push(tile)
             }
         }
-
-        return positions
     }
 
-    //return true if the two positions are the same
-    function positionMatch(a,b){
-        return a.x === b.x && a.y === b.y
+    return tiles
+}
+
+//function to generate Mines positions
+function getMinePositions(boardSize, bombAmount){
+    const positions = []
+
+    //Loop until enough Mines
+    while (positions.length < bombAmount){ 
+        const position = {
+            x: random(boardSize),
+            y: random(boardSize),
+        };
+
+        if (!positions.some(p => positionMatch(p,position))){
+            positions.push(position);
+        }
     }
 
-    //return a random integer
-    function random(boardSize){ 
-        return Math.floor(Math.random() * boardSize)
-    }
-})
+    return positions
+}
+
+//return true if the two positions are the same
+function positionMatch(a,b){
+    return a.x === b.x && a.y === b.y
+}
+
+//return a random integer
+function random(size){ 
+    return Math.floor(Math.random() * size);
+}
+
+init()
