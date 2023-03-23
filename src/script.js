6f2fd24ec2c786c3acc63343cf59489a1d588972
bia3334 <quankun3357@gmail.com>
2023-03-23 20:59:51 +0700

    xong phan win lose

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@
     // a. reveal titles (Done)
 // 3. Right click on titles
     // a. Mark tiles (Partially done)
-// 4. check for win/lose
+// 4. check for win/lose (Done)
 
 const TILE_STATUSES = {
     HIDDEN: "hidden",
