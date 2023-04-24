2a447580721295cd6b8ce31401340c6ff9d888eb
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-24 08:58:42 +0700

    test webhook

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@
     // a. Mark tiles (Partially done)
     // b. Decrease the number of mines left on the display (done)
 // 4. check for win/lose (Partially Done)
+// 5. Make the first move to be always safe
 
 const TILE_STATUSES = {
     HIDDEN: "hidden",
