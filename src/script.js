bf402838a20161783b97c959221cb8f73f91aaa4
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-24 08:46:10 +0700

    update the list of works

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@
     // a. reveal titles (Done)
 // 3. Right click on titles
     // a. Mark tiles (Partially done)
-    // b. Decrease the number of mines left on the display
+    // b. Decrease the number of mines left on the display (done)
 // 4. check for win/lose (Partially Done)
 
 const TILE_STATUSES = {
