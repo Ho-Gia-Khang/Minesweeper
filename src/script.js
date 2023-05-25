2b44e201be60895cca849ca495a245a534c7f50b
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-05-25 13:16:45 +0700

    it was adjusted by the prettier extension

--- src/script.js
+++ src/script.js
@@ -1,19 +1,19 @@
 // danh sach cong viec du kien:
 // 1. Populate a board with tiles/mines (done)
-// 2. Left click on the titles
-    // a. reveal titles (Done)
-// 3. Right click on titles
-    // a. Mark tiles (Partially done)
-    // b. Decrease the number of mines left on the display (done)
+// 2. Left click on the titles -> reveal titles (Done)
+// 3. Right click on titles:
+// 3a. Mark tiles (Partially done)
+// 3b. Decrease the number of mines left on the display (done)
 // 4. check for win/lose (Partially Done)
 // 5. Make the first move to be always safe
+// 6. Make undo function
 
 const TILE_STATUSES = {
     HIDDEN: "hidden",
     MINE: "mine",
     NUMBER: "number",
-    MARKED: "marked"
-}
+    MARKED: "marked",
+};
 
 // varialbles declaration
 let boardSize = 8;
@@ -36,24 +36,24 @@ generate(board, boardElement, boardSize);
 
 // change the board size
 let data;
-const dropdown = document.getElementById('board-size');
-dropdown.addEventListener('change', (e) => {
+const dropdown = document.getElementById("board-size");
+dropdown.addEventListener("change", (e) => {
+    win;
     data = e.target.value;
     console.log(data);
-    switch(data){
-        case '8x8':
+    switch (data) {
+        case "8x8":
             boardSize = 8;
             NUMBER_OF_MINES = 15;
             break;
-        case '16x16':
+        case "16x16":
             boardSize = 16;
             NUMBER_OF_MINES = 40;
             break;
-        case '20x20':
+        case "20x20":
             boardSize = 20;
             NUMBER_OF_MINES = 65;
-            break;          
-            
+            break;
     }
 
     const board = createBoard(boardSize, NUMBER_OF_MINES);
@@ -64,36 +64,36 @@ dropdown.addEventListener('change', (e) => {
 });
 
 // generate the board
-function generate(board, boardElement, boardSize){
+function generate(board, boardElement, boardSize) {
     startSound.play();
-    board.forEach(row => {
-        row.forEach(tile => {
-            boardElement.append(tile.element)
+    board.forEach((row) => {
+        row.forEach((tile) => {
+            boardElement.append(tile.element);
             tile.element.addEventListener("click", () => {
                 revealTile(board, tile);
                 checkGameEnd(board);
-            })
-            tile.element.addEventListener("contextmenu", e => {
+            });
+            tile.element.addEventListener("contextmenu", (e) => {
                 e.preventDefault();
                 markTile(tile);
                 listMinesLeft(board);
-            })
-        })
-    })
+            });
+        });
+    });
     boardElement.style.setProperty("--size", boardSize);
 }
 
-// re-render the board 
-function render(board, boardElement, boardSize){
+// re-render the board
+function render(board, boardElement, boardSize) {
     startSound.pause();
     lostSound.pause();
-    document.getElementById('board').innerText = ''; // delete the old board
+    document.getElementById("board").innerText = ""; // delete the old board
     // render the new board
-    generate(board,boardElement,boardSize);
+    generate(board, boardElement, boardSize);
 }
 
 // function to create the board
-function createBoard(boardSize, numberOfMines){
+function createBoard(boardSize, numberOfMines) {
     const board = [];
     const minePositions = getMinePositions(boardSize, numberOfMines);
 
@@ -110,78 +110,85 @@ function createBoard(boardSize, numberOfMines){
                 y,
                 mine: minePositions.some(positionMatch.bind(null, { x, y })),
                 get status() {
-                    return this.element.dataset.status
+                    return this.element.dataset.status;
                 },
                 set status(value) {
-                    this.element.dataset.status = value
+                    this.element.dataset.status = value;
                 },
             };
 
-            row.push(tile)
+            row.push(tile);
         }
         board.push(row);
     }
-    
+
     return board;
-};
+}
 
-// function to mark tile 
-function markTile(tile){
-    if(tile.status !== TILE_STATUSES.HIDDEN && 
-        tile.status !== TILE_STATUSES.MARKED){
-        return //end if it's already reveal
+// function to mark tile
+function markTile(tile) {
+    if (
+        tile.status !== TILE_STATUSES.HIDDEN &&
+        tile.status !== TILE_STATUSES.MARKED
+    ) {
+        return; //end if it's already reveal
     }
 
-    if (tile.status === TILE_STATUSES.MARKED){
-        tile.status = TILE_STATUSES.HIDDEN //if it's marked unmark it
-    }
-    else {tile.status = TILE_STATUSES.MARKED} //mark it otherwise
+    if (tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN; //if it's marked unmark it
+    } else {
+        tile.status = TILE_STATUSES.MARKED;
+    } //mark it otherwise
 }
 
 //function to reveal the clicked tile
-function revealTile(board, tile){
-    if (tile.status !== TILE_STATUSES.HIDDEN){
-        return //end if tile is already reveal
+function revealTile(board, tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN) {
+        return; //end if tile is already reveal
     }
 
-    if(tile.mine){
+    if (tile.mine) {
         tile.status = TILE_STATUSES.MINE;
-        return //end if the tile is a mine
+        return; //end if the tile is a mine
     }
 
     tile.status = TILE_STATUSES.NUMBER;
-    const nearbyTiles = adjacentTiles(board,tile);
-    const mines = nearbyTiles.filter(t => t.mine);
+    const nearbyTiles = adjacentTiles(board, tile);
+    const mines = nearbyTiles.filter((t) => t.mine);
 
-    if (mines.length === 0){ //reveal nearby tiles that are not adjacent to any mines
-        nearbyTiles.forEach(revealTile.bind(null,board));
-    }
-    else{ //show number of mines nearby
+    if (mines.length === 0) {
+        //reveal nearby tiles that are not adjacent to any mines
+        nearbyTiles.forEach(revealTile.bind(null, board));
+    } else {
+        //show number of mines nearby
         tile.element.textContent = mines.length;
     }
 }
 
 // function to calculate the number of bombs surround the square
-function adjacentTiles(board, {x,y}){
-    const tiles = []
+function adjacentTiles(board, { x, y }) {
+    const tiles = [];
 
-    for(let xOff = -1; xOff<=1; xOff++){ //nearby postions on left and right
-        for(let yOff = -1; yOff<=1; yOff++){ //nearby positions upper and bellow
-            const tile = board[x+xOff]?.[y+yOff]
-            if (tile){
-                tiles.push(tile)
+    for (let xOff = -1; xOff <= 1; xOff++) {
+        //nearby postions on left and right
+        for (let yOff = -1; yOff <= 1; yOff++) {
+            //nearby positions upper and bellow
+            const tile = board[x + xOff]?.[y + yOff];
+            if (tile) {
+                tiles.push(tile);
             }
         }
     }
 
-    return tiles
+    return tiles;
 }
 
 // display the number of mine left
-function listMinesLeft(board){
+function listMinesLeft(board) {
     const markedTilesCount = board.reduce((count, row) => {
         return (
-            count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+            count +
+            row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
         );
     }, 0);
 
@@ -189,93 +196,96 @@ function listMinesLeft(board){
 }
 
 //function to generate Mines positions
-function getMinePositions(boardSize, bombAmount){
-    const positions = []
+function getMinePositions(boardSize, bombAmount) {
+    const positions = [];
 
     //Loop until enough Mines
-    while (positions.length < bombAmount){ 
+    while (positions.length < bombAmount) {
         const position = {
             x: random(boardSize),
             y: random(boardSize),
         };
 
-        if (!positions.some(p => positionMatch(p,position))){
+        if (!positions.some((p) => positionMatch(p, position))) {
             positions.push(position);
         }
     }
 
-    return positions
+    return positions;
 }
 
 //return true if the two positions are the same
-function positionMatch(a,b){
-    return a.x === b.x && a.y === b.y
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y;
 }
 
 //return a random integer
-function random(size){ 
+function random(size) {
     return Math.floor(Math.random() * size);
 }
 
 //function to check game is over
 function checkGameEnd(board) {
-    const win = checkWin(board)
-    const lose = checkLose(board)
+    const win = checkWin(board);
+    const lose = checkLose(board);
     //Ngăn người dùng thao tác thêm
     if (win || lose) {
-        boardElement.addEventListener('click', stopProp, {capture: true})
-        boardElement.addEventListener('contextmenu', stopProp, {capture: true})
+        boardElement.addEventListener("click", stopProp, { capture: true });
+        boardElement.addEventListener("contextmenu", stopProp, {
+            capture: true,
+        });
     }
     //Thắng
     if (win) {
-        messageText.textContent = "You Win !!!"
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        messageText.textContent = "You Win !!!";
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
     //Thua
     if (lose) {
         startSound.pause();
-        messageText.textContent = "You Lost :<<"
+        messageText.textContent = "You Lost :<<";
         //Hàm này để hiện tất cả các mìn
-        board.forEach(row => {
-            row.forEach(tile => {
-                if(tile.status === TILE_STATUSES.MARKED) markTile(tile)
-                if(tile.mine) revealTile(board,tile)
-           }) 
-        })
+        board.forEach((row) => {
+            row.forEach((tile) => {
+                if (tile.status === TILE_STATUSES.MARKED) markTile(tile);
+                if (tile.mine) revealTile(board, tile);
+            });
+        });
         lostSound.play();
     }
 }
 
 function stopProp(e) {
-    e.stopImmediatePropagation()
+    e.stopImmediatePropagation();
 }
 //Thắng
-function checkWin (board) {
-    return board.every(row => {
-        return row.every(tile => {
-            return tile.status === TILE_STATUSES.NUMBER || 
-            (tile.mine && // Nói tóm gọn là nếu số mìn bị dấu = số mìn ban đầu khi đã hết lượt bấm thì thắng
-                (tile.status === TILE_STATUSES.HIDDEN ||
-                    tile.status === TILE_STATUSES.MARKED))
-        })
-    })
-
+function checkWin(board) {
+    return board.every((row) => {
+        return row.every((tile) => {
+            return (
+                tile.status === TILE_STATUSES.NUMBER ||
+                (tile.mine && // Nói tóm gọn là nếu số mìn bị dấu = số mìn ban đầu khi đã hết lượt bấm thì thắng
+                    (tile.status === TILE_STATUSES.HIDDEN ||
+                        tile.status === TILE_STATUSES.MARKED))
+            );
+        });
+    });
 }
- //Thua
-function checkLose (board) {
-    return board.some(row => {
-        return row.some(tile => {
-            return tile.status === TILE_STATUSES.MINE //Có mìn thì thua
-        })
-    })
+//Thua
+function checkLose(board) {
+    return board.some((row) => {
+        return row.some((tile) => {
+            return tile.status === TILE_STATUSES.MINE; //Có mìn thì thua
+        });
+    });
 }
 
 //Reset cái page lại
 function resetGame() {
     location.reload();
-  }
-  
+}
+
 const resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", function () {
     location.reload();
 });
